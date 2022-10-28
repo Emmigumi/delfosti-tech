df51dcd9dc30bd0bb9d715aae2b8ffd2c6b54093
Jammie Hurtado <98611521+Emmigumi@users.noreply.github.com>
2022-10-28 17:05:08 -0500

    add pagination and sort

--- main.js
+++ main.js
@@ -4,20 +4,57 @@ const dataProducts = document.getElementById('data-products');
 const productList =  document.getElementsByTagName('td');
 //Botones de paginación
 const btnConteiner = document.getElementsByClassName('buttons');
+const contador = document.getElementById('contador');
+const btnPrev = document.getElementById('prev');
+const btnNext = document.getElementById('next');
+//Botones de ordenamiento
+const btnSortName = document.getElementById('sort');
 // buscador
 const inputSearch = document.querySelector('#searchInput');
-const trList = document.getElementsByTagName("tr");
-
+const rowList = document.getElementsByTagName("tr");
+//link de API
+ let url = 'http://localhost:3000/products?_page=1&_limit=5';
 
 window.addEventListener('DOMContentLoaded', () => {
     dataProducts.innerHTML = "<h1>Loading</h1>";
-    loaderProducts();
+    loaderProducts(url);
 }) 
+//paginación
+//Añadiendo funcionalidad de retorno al botonPrev
+let count = 1;
+btnNext.addEventListener('click', e=>{
+    if(count===4){
+
+    } else{
+        count++;  
+        contador.innerHTML = count;
+       let url = `http://localhost:3000/products?_page=${count}&_limit=5`;
+       loaderProducts(url);
+       console.log(url);
+    }
+})
+
+btnPrev.addEventListener('click', e=>{
+   if(count===1){
+
+   } else{
+    count--;
+    contador.innerHTML = count;
+    let url = `http://localhost:3000/products?_page=${count}&_limit=5`;
+    loaderProducts(url);
+   }
+})
+
+//Funcionalidad de ordenamiento por nombre
+btnSortName.addEventListener('click', e=>{
+    let url = `http://localhost:3000/products?_page=${count}&_sort=name&_limit=5`;
+    loaderProducts(url);
+})
 
 const getProducts = (data) => {
     let body = '';
     for (let i=0; i<data.length; i++){
-        body += `<tr class="tr-list">
+        body += `<tr class="row-list">
         <th class="product">${data[i].id}</th>
         <td class="product">${data[i].name}</td>
         <th class="product">${data[i].slug}</th>
@@ -27,21 +64,20 @@ const getProducts = (data) => {
     dataProducts.innerHTML = body
 }
 
-function loaderProducts(){
-    let url = 'http://localhost:3000/products';
+function loaderProducts(url){
     fetch(url)
     .then(response=> response.json())
     .then(data => getProducts(data))
     .catch(error => console.log(error));
 }
-//Evento keyup que filtra a partir de la 4 letra de cada fila: nombre
+//Evento keyup que filtrar a partir de la 4 letra de cada fila: nombre
 inputSearch.addEventListener('keyup', e => {
     let inputTextValue = e.target.value;
     console.log(inputTextValue)
     let er = new RegExp(inputTextValue, "i")
-    for (let j = 1; j<trList.length; j++){
-        let valorTr = trList[j];
-   //     console.log('soy trL[j]', valorTr.children[1].innerText)
+    for (let j = 1; j<rowList.length; j++){
+        let valorTr = rowList[j];
+   //     console.log(valorTr.children[1].innerText)
         if(er.test(valorTr.children[1].innerText)&&inputTextValue.length>3){
             valorTr.classList.remove('filter')
         } else {
@@ -49,8 +85,3 @@ inputSearch.addEventListener('keyup', e => {
         }
     }       
 })
-
-//paginación
-//Botones
-//btnConteiner
-
